708aba4ddea074387dcd5fd2ca598d5323d47bef
irslan <newpc20@gmail.com>
2025-07-15 20:27:09 +0500

    Update Product.js

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -24,7 +24,7 @@ const productSchema = new mongoose.Schema({
   category: {
     type: String,
     required: true,
-    enum: ['electronics', 'clothing', 'beauty', 'home', 'other', 'candies', 'biscuits', 'drinks' 'makeup']
+    enum: ['electronics', 'clothing', 'beauty', 'home', 'other', 'candies', 'biscuits', 'drinks', 'makeup']
   },
   stock: {
     type: Number,
